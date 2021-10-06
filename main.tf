e835043fc8c84396ee29feb01ec719bea6f04a03
nithinsaju <51135252+nithinsaju@users.noreply.github.com>
2021-10-06 18:02:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "my-ec2" {
+  #name = ""
+  instance_type = "t2.micro"
+  ami = "ami-087c17d1fe0178315"
+  #vpc_id  = "vpc-01d2999949224872a"
+  subnet_id = "subnet-06f06aa686014295b"
+}
