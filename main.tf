77d03c503e30a418cfb452fd3f417d8fc3ea9879
nithinsaju <51135252+nithinsaju@users.noreply.github.com>
2021-10-07 14:20:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,5 @@ provider "aws" {
 
 resource "aws_instance" "my-ec2" {
   instance_type = "t2.micro"
-  ami = "ami-087c17d1fe0178315"
-  subnet_id = "subnet-06f06aa686014295b"
+  ami = "ami-041d6256ed0f2061c"
 }
