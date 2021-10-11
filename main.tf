ebe9aed4e4a7493ef747281fffe14c2999858d98
nithinsaju <51135252+nithinsaju@users.noreply.github.com>
2021-10-11 10:40:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,10 @@ provider "aws" {
 resource "aws_instance" "u15-aws-ec2" {
   instance_type = "t2.micro"
   ami = "ami-041d6256ed0f2061c" 
-  name = var.instance_name
+  
+  tags = {
+    Name = var.instance_name
+  }
 }
 
 variable "instance_name" {
